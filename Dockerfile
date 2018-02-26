91a3a5474cbd467f73b4a7eb43f8ef8aeba14f66
Ambrose Chua <ambrose@chua.family>
2018-02-26 23:14:06 +0800

    Example of embedding version number on build

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY . .
 ENV CGO_ENABLED=0
 ENV GOOS=linux
 ENV GOARCH=amd64
-RUN go build -ldflags '-extldflags "-static"' -o short
+RUN go build -ldflags '-extldflags "-static" -X main.version=1.0' -o short
 
 
 FROM scratch
--- healthz.go
+++ healthz.go
@@ -6,11 +6,11 @@ import (
 	"runtime"
 )
 
-var ver string
+var version string
 
 func healthz(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintf(w, "ok\n")
-	fmt.Fprintf(w, "ver: %v\n", ver)
+	fmt.Fprintf(w, "ver: %v\n", version)
 	fmt.Fprintf(w, "go ver: %v\n", runtime.Version())
 }
