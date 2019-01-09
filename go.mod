5b8692a93084f8078bc29db73c687f9e17cc8f77
Ambrose Chua <ambrose@chua.family>
2019-01-09 14:39:44 +0800

    Revert mistakes

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module "github.com/serverwentdown/short"
+module github.com/serverwentdown/short
 
 require (
-	"github.com/julienschmidt/httprouter" v0.0.0-20150626000000-d18983907793
-	"github.com/lib/pq" v0.0.0-20180201184707-d34b9ff171c2
+	github.com/julienschmidt/httprouter v0.0.0-20150626000000-d18983907793
+	github.com/lib/pq v0.0.0-20180201184707-d34b9ff171c2
 )
--- router.go
+++ router.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"net/http"
-	
 	"github.com/julienschmidt/httprouter"
 )
 
 func NewRouter(h *Handlers) *httprouter.Router {
 	router := httprouter.New()
-	router.GET("/healthz", func(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-		healthz(w)
-	})
 	router.POST("/new", h.Create)
 	router.GET("/", h.Index)
 	router.GET("/:id", h.Get)
